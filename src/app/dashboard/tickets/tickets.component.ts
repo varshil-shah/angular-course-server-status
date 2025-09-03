d35042a84645fa09b82401a403bbf39c0456e346
Varshil Shah <varshilshah.in@gmail.com>
2025-09-03 22:00:23 +0530

    Initial commit

---
+++ src/app/dashboard/tickets/tickets.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { NewTicketsComponent } from './new-tickets/new-tickets.component';
+
+@Component({
+  selector: 'app-tickets',
+  standalone: true,
+  imports: [NewTicketsComponent],
+  templateUrl: './tickets.component.html',
+  styleUrl: './tickets.component.css',
+})
+export class TicketsComponent {}
