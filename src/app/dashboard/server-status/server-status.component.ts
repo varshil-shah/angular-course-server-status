1ef3911500619193e273653286d1796ffcc59f83
Varshil Shah <varshilshah.in@gmail.com>
2025-09-04 23:36:05 +0530

    Learn template variable

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,6 +7,32 @@ import { Component, ViewEncapsulation } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
 })
-export class ServerStatusComponent {
-  currentStatus = 'online';
+export class ServerStatusComponent implements OnInit, OnDestroy, AfterViewInit {
+  currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+
+  private interval?: ReturnType<typeof setInterval>;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.interval = setInterval(() => {
+      const rnd = Math.random(); // 0 - 0.999999
+
+      if (rnd < 0.5) {
+        this.currentStatus = 'online';
+      } else if (rnd < 0.9) {
+        this.currentStatus = 'offline';
+      } else {
+        this.currentStatus = 'unknown';
+      }
+    }, 5000);
+  }
+
+  ngAfterViewInit(): void {
+    console.log('AFTER VIEW INIT');
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.interval);
+  }
 }
--- src/app/dashboard/tickets/new-tickets/new-tickets.component.ts
+++ src/app/dashboard/tickets/new-tickets/new-tickets.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-tickets',
   standalone: true,
-  imports: [ButtonComponent, ControlComponent],
+  imports: [ButtonComponent, ControlComponent, FormsModule],
   templateUrl: './new-tickets.component.html',
   styleUrl: './new-tickets.component.css',
 })
-export class NewTicketsComponent {}
+export class NewTicketsComponent {
+  onSubmit(titleInput: HTMLInputElement) {
+    const enteredTitle = titleInput.value;
+    console.log('Entered title:', enteredTitle);
+  }
+}
